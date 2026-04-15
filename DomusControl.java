88ee49d7365e496675bb0401d95086bfaafbf56b
diogomvieira06 <dv7diogo@gmail.com>
2026-04-15 17:07:57 +0100

    falta acabar algumas funçoes

--- DomusControl.java
+++ DomusControl.java
@@ -21,9 +21,28 @@ public class DomusControl {
             System.out.println("0. Sair");
             System.out.print("Opção: ");
 
-            //int opçao = sc.parseInt(sc.nextInt());
-
-
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 1 -> criarUtilizador();
+                case 2 -> criarCasa();
+                case 3 -> criarDivisao();
+                case 4 -> adicionarDispositivo();
+                case 5 -> listarUtilizadores();
+                case 6 -> listarCasas();
+                case 7 -> controlarDispositivo();// ver melhor dps
+                case 0 -> sair = true;
+                default -> System.out.println("Opção inválida ");
+            }
         }
+        sc.close(); // Fechar o Scanner ao final do programa
+    }
+
+    private static void criarUtilizador(){
+        System.out.println("ID do utilizador: ");
+        int id = sc.nextInt();
+        System.out.print("Nome do utilizador: ");
+        String nome = sc.next();
+        utilizadores.add(new Utilizador(id, nome));
+        System.out.println("Utilizador criado com sucesso!");
     }
 }
